b94fe63592b95e8dfbd44a3d1a0d63f6bc0f30af
GuangJie1 <168152702+GuangJie1@users.noreply.github.com>
2025-03-29 15:12:48 +0800

    Create run.sh

---
+++ 3rdparty/maven/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+current_dir=$(cd "$(dirname "$0")" && pwd)
+software_list=$current_dir/software-list
+result_path="/var/run/maven/"
+
+packages=""
+if [[ -f "$software_list" ]]; then
+    packages=$(grep -v '^#' "$software_list" | tr '\n' ' ')
+    echo -e "Using software-list: ${args}"
+else
+    echo -e "Warning: software-list not found in $script_dir"
+fi
+
+# the result is saved in `result_path`
+docker run -d -e RESULT_PATH=${result_path} -v ${result_path}:${result_path} openeuler/maven:latest $packages
